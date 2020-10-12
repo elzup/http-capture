82e302effbaa4e44069f08b28da8a5ca5570bb51
elzup <guild0105@gmail.com>
2020-10-12 10:39:05 +0900

    all request receive

--- app.js
+++ app.js
@@ -6,16 +6,6 @@ app.use(express.text())
 app.use(middleware)
 
 function middleware(req, res, next) {
-  const chunks = []
-
-  req.on('data', (chunk) => chunks.push(chunk.toString()))
-  req.on('end', function () {
-    req.rawBody = chunks.join()
-    next()
-  })
-}
-
-app.post('/path', (req, res) => {
   console.log('---')
   console.log('-- Host')
   console.log(req.hostname)
@@ -23,13 +13,17 @@ app.post('/path', (req, res) => {
   console.log(req.headers)
   console.log('-- Query Params')
   console.log(req.query)
-  console.log('-- rawBody')
-  console.log(req.rawBody)
-  console.log('---')
+  const chunks = []
 
-  const data = { ok: true }
-  res.send(JSON.stringify(data))
-})
+  req.on('data', (chunk) => chunks.push(chunk.toString()))
+  req.on('end', function () {
+    req.rawBody = chunks.join()
+    console.log('-- rawBody')
+    console.log(req.rawBody)
+    console.log('---')
+    next()
+  })
+}
 
 app.listen(port, () => {
   console.log(`app listening at http://localhost:${port}`)
